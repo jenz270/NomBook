c09603f9cf526c5149b2940cf55f1356d68097b0
Jieni Hou <jienihou@Jienis-MacBook-Pro.local>
2020-03-28 19:08:49 -0400

    Add HomeFragment changes

--- app/src/main/java/com/jenzhouu/nombook/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jenzhouu/nombook/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.widget.SearchView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +14,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jenzhouu.nombook.R
 import com.jenzhouu.nombook.ui.TopSpacingItemDecoration
+import android.content.Intent
+import android.net.Uri
+
 
 class HomeFragment : Fragment() {
 
@@ -26,9 +30,16 @@ class HomeFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val randomizeButton = view.findViewById<Button>(R.id.randomize_button)
         val recyclerView = view.findViewById<RecyclerView>(R.id.top_ten_recipes_rv)
         val topRecipesAdapter = TopRecipesAdapter()
         val searchView = view.findViewById<SearchView>(R.id.search_view)
+
+        randomizeButton.setOnClickListener {
+            val uri = Uri.parse("http://www.africau.edu/images/default/sample.pdf") // missing 'http://' will cause crashed
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            context?.startActivity(intent)
+        }
 
         searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
